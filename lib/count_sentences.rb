2d0c5d7bd3a800b1db45275fe4a86a0dd22bc3af
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-23 17:03:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/[?!.]\s+[A-Z]/).count
-    return count
+    #self.split(/[?!.]\s+[A-Z]/).count
+    self.split(/[.!?]+/).count
   end
 end
